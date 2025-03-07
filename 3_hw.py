72e0cb10262d8b328d932fbb16f2d842b4c102f7
nadin-88 <nadin-88@yandex.ru>
2025-03-07 20:12:30 +0300

    Update 3_hw.py

--- 3_hw.py
+++ 3_hw.py
@@ -1,32 +1,39 @@
 #2. Функция на вход получает два произвольных числа. Вывести в консоль наибольшее из чисел.
-        def biggest_number (a,b):
-            if a > b:
-                print(a)
-            elif b > a:
-                print (b)
-        biggest_number(25, 46)
+def biggest_number(a, b):
+    if a > b:
+        print(a)
+    elif b > a:
+        print(b)
+
+
+biggest_number(25, 46)
+
 
 
 #3 Функция на вход получает два произвольных числа. Вывести в консоль “yes”, если они отличаются друг от друга на 135, иначе вывести на экран “No”
-        def num_difference(a,b):
-            if (a-b) == 135:
-                print( 'Yes')
-            else:
-                print('No')
-        num_difference(271,136)
+def num_difference(a, b):
+    if (a - b) == 135:
+        print('Yes')
+    else:
+        print('No')
+
+
+num_difference(271, 136)
 
 
 #4. Функция на вход получает произвольное число от 1 до 12 (номер месяца). Вывести название сезона года в консоль (зима, весна, лето, осень)
-        def seasons(month_number):
-            if month_number == 12 or month_number == 1 or month_number == 2:
-                print( 'Зима')
-            elif month_number in range(3,6):
-                print('Весна')
-            elif month_number in range (6,9):
-                print('Лето')
-            elif month_number in range (9, 12):
-                print('Осень')
-        seasons(7)
+def seasons(month_number):
+    if month_number == 12 or month_number == 1 or month_number == 2:
+        print('Зима')
+    elif month_number in range(3, 6):
+        print('Весна')
+    elif month_number in range(6, 9):
+        print('Лето')
+    elif month_number in range(9, 12):
+        print('Осень')
+
+
+seasons(7)
 
 
 #5.Функция на вход получает три произвольных числа. Если все числа больше 10, то вывести на экран “yes”, иначе “no”;
